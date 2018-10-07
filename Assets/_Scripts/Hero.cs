041d55d2dad1a7f6bfc4181ec99839c6805e09d8
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-10-07 18:42:41 -0400

    Background Set and Player Added
    Camera now has a custom aspect ratio and displays a black background. Player with seperate hitbox added along with materials. Player moves and rotates when arrow keys are pressed. Shift key can be held down to enter focus mode.

---
+++ Assets/_Scripts/Hero.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float speedFocus = 10;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public bool isFocusMode = false;
+    public float shieldLevel = 1;
+
+	// Use this for initialization
+	void Awake()
+    {
+		if(S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        if (Input.GetKey("right shift") == true || Input.GetKey("left shift") == true)
+        {
+            isFocusMode = true;
+        }
+        else
+        {
+            isFocusMode = false;
+        }
+
+        Vector3 pos = transform.position;
+        if(isFocusMode == true)
+        {
+            pos.x += xAxis * speedFocus * Time.deltaTime;
+            pos.y += yAxis * speedFocus * Time.deltaTime;
+        }
+        else
+        {
+            pos.x += xAxis * speed * Time.deltaTime;
+            pos.y += yAxis * speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+	}
+}
---
+++ Assets/_Scripts/Hitbox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public GameObject Player;
+    public float startX;
+    public float startY;
+    public float startZ;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = Player.transform.position;
+        newPos.x -= startX;
+        newPos.y -= startY;
+        newPos.z -= startZ;
+        transform.position = newPos;
+	}
+}
